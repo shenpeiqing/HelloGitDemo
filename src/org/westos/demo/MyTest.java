d4909ea53b133958d2286e258dd612e392e4d61a
master <284309510@qq.com>
2018-07-15 13:50:01 +0800

    提交到分支1

--- src/org/westos/demo/MyTest.java
+++ src/org/westos/demo/MyTest.java
@@ -3,5 +3,6 @@ package org.westos.demo;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("heheheh");
+        System.out.println("分支1新加的代码");
     }
 }
