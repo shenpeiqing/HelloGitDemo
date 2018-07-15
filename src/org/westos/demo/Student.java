af45737261059ed671bad30f0b6b03f376aa8e03
master <284309510@qq.com>
2018-07-15 13:45:17 +0800

    第三次提交

--- src/org/westos/demo/Student.java
+++ src/org/westos/demo/Student.java
@@ -1,4 +1,22 @@
 package org.westos.demo;
 
 public class Student {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
